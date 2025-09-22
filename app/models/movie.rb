d5ca33e56cc6b1319cddd9451c857a4f5caa5a65
Skyler1216 <swhaitrao@gmail.com>
2025-09-22 01:58:20 -0500

    映画の新規登録ページをリファクタリングし、Tailwind CSSを使用してスタイルを統一しました。エラーメッセージの表示を改善し、画像URLの入力フィールドを追加しました。また、ユーザー体験を向上させるために、ページのレイアウトを整理しました。

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,6 @@
 class Movie < ApplicationRecord
   validates :name, presence: true, uniqueness: true
   validates :year, presence: true
+  validates :image_url, presence: true
   has_many :schedules, dependent: :destroy
 end
